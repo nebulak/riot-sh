c09c4b3ea589b33fa83d2b1ee4b98508650544f7
nebulak <nebulak@openmailbox.org>
2018-05-09 12:36:22 +0200

    update

--- riot.sh
+++ riot.sh
@@ -1,6 +1,9 @@
 # Base system: debian stretch
 # Debian package search: https://packages.debian.org/search?searchon=names&keywords=gajim-omemo
-
+SCRIPT_NAME="$0"
+ARGS="$@"
+UPDATE_URL="https://nebulak.github.io/riot-sh"
+VERSION="0.0.0"
 COMMAND=$1
 
 case "$1" in
@@ -9,9 +12,12 @@ case "$1" in
             ;;
 
         update)
-            install # update all packages
+            install # update core and packages
             ;;
 
+        post-update)
+            update # update core and packages
+            ;;
         uninstall)
             uninstall # uninstall one or all packages
             ;;
@@ -117,3 +123,47 @@ install() {
   # //TODO: delete
   # torbrowser-launcher
 }
+
+# update riot script
+# source: https://bani.com.br/2013/04/shell-script-that-updates-itself/
+check_upgrade () {
+
+  # check if there is a new version of this file
+  # here, hypothetically we check if a file exists in the disk.
+  # it could be an apt/yum check or whatever...
+  [ -f "$NEW_FILE" ] && {
+
+    # install a new version of this file or package
+    # again, in this example, this is done by just copying the new file
+    echo "Found a new version of me, updating myself..."
+    cp "$NEW_FILE" "$SCRIPT_NAME"
+    rm -f "$NEW_FILE"
+
+    # note that at this point this file was overwritten in the disk
+    # now run this very own file, in its new version!
+    echo "Running the new version..."
+    $SCRIPT_NAME $ARGS
+
+    # now exit this old instance
+    exit 0
+  }
+
+  echo "I'm VERSION $VERSION, already the latest version."
+}
+
+update() {
+  wget_update()
+
+}
+
+wget_update() {
+  sudo wget "$UPDATE_URL/version"
+  sudo wget "$UPDATE_URL/riot.sh"
+  # //TODO: move to install location
+  # //TODO: run new instance
+  # echo "Running the new version..."
+  # $SCRIPT_NAME $ARGS
+}
+
+# //TODO: do semver check of version before update
+# source: https://github.com/fsaintjacques/semver-tool/blob/master/src/semver
