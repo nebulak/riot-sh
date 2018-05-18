464c7db665608079fde75413f1af67d14ec13503
nebulak <nebulak@users.noreply.github.com>
2018-05-18 10:04:08 +0200

    Update riot.sh

--- riot.sh
+++ riot.sh
@@ -48,6 +48,12 @@ install() {
 
   sudo apt-get install jq -y
 
+  ################ Install ansible ########################
+  
+  deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main
+  sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+  sudo apt-get update
+  sudo apt-get install ansible
 
   ################ Add backports-repo #####################
 
